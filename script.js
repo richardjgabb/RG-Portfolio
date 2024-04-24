f34732584bab25d1723771a071491b8cf0c9f974
richardjgabb <richardjgabb@.com>
2024-04-24 14:03:01 +0100

    animations

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ const projectImage = document.querySelectorAll('.hidden');
 projectImage.forEach(card => {
     observer.observe(card);
 })
+
+const aboutObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const aboutSection = document.querySelectorAll('.reveal');
+aboutSection.forEach(card => {
+    aboutObserver.observe(card);
+})
